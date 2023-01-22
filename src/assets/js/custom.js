c1ad127298416b0b1a71e030b901309c735ebc78
bor1s-oc <123120655+bor1s-oc@users.noreply.github.com>
2023-01-23 00:19:48 +0200

    timeout for svg loader

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,29 +1,32 @@
 window.addEventListener('load', function () {
 
     // scroll spy and start animation
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            var blinkItem = entry.target.querySelectorAll('path');
-            if (entry.isIntersecting) {
-                blinkEf(blinkItem, 10);
-                if (entry.target.classList.contains('comet')) {
-                    blinkEf(blinkItem, 2);
+
+    setTimeout( () => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                var blinkItem = entry.target.querySelectorAll('path');
+                if (entry.isIntersecting) {
+                    blinkEf(blinkItem, 7);
+                    if (entry.target.classList.contains('comet')) {
+                        blinkEf(blinkItem, 2);
+                    }
                 }
-            }
+            });
         });
-    });
-    
-    const scrollSpyElements = document.querySelectorAll('.scroll-spy')
-    scrollSpyElements.forEach((el) => observer.observe(el));
-    
-    // white blinks effect
-    function blinkEf(blinkItems, timeOut) {
-        blinkItems.forEach((element, blinkItems) => {
-            setTimeout( () => {
-                element.classList.add('white-blink--active');
-            }, blinkItems * timeOut);
-        });  
-    }
+        
+        const scrollSpyElements = document.querySelectorAll('.scroll-spy');
+        scrollSpyElements.forEach((el) => observer.observe(el));
+        
+        // white blinks effect
+        function blinkEf(blinkItems, timeOut) {
+            blinkItems.forEach((element, blinkItems) => {
+                setTimeout( () => {
+                    element.classList.add('white-blink--active');
+                }, blinkItems * timeOut);
+            });  
+        }
+    }, 500);
 
     // country-slider
     var slider = tns({
