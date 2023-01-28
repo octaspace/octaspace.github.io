dcfe2d3f63cfe9811da408f81478e3938fe7ec45
x9q <x9q@x9q.com>
2023-01-29 01:12:15 +0200

    Add roadmap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
 import { AppComponent } from './app.component';
+import { RoadmapComponent } from './components/roadmap/roadmap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoadmapComponent
   ],
   imports: [
     BrowserModule,
